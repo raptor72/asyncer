91c464798e5a385c71e187e945fee31e2971f311
raptor72 <mctaras51@yandex.ru>
2020-07-15 11:14:56 +0400

    Add coroutines and delegated generator

---
+++ 06_delegate.py
@@ -0,0 +1,37 @@
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+
+class BlaBlaException(Exception):
+    pass
+
+
+# @coroutine
+def subgen():
+    while True:
+        try:
+            message = yield
+        except StopIteration:
+            # print('Ku-Ku!!!')
+            break
+        else:
+            print('...........', message)
+    return 'Returned from subgen()'
+
+
+@coroutine
+def delegator(g):
+    # while True:
+    #     try:
+    #         data = yield
+    #         g.send(data)
+    #     except BlaBlaException as e:
+    #         g.throw(e)
+    result = yield from g
+    print(result)
+
+
